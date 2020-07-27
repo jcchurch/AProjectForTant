331fa7eebc834b195cbf83e29d43630667f057f7
James Church <jcchurch@gmail.com>
2020-07-27 03:19:47 -0500

    It reads in the full file completely.

--- ForTant.f95
+++ ForTant.f95
@@ -16,59 +16,71 @@ contains
 subroutine pull_data_from_file(unit)
     implicit none
 
-    integer :: unit
+    integer :: unit, reason, i
 
     character (len=12) :: cStnid, cClouds, cUnknown, cUnknown2
     integer :: iCurYear, iCurMon, iCurDay, iCurHour, iRTime, iNewNLvls
     integer :: iLat, iLon, iElev
 
-    read (unit,*) cStnid, iCurYear, iCurMon, iCurDay, iCurHour, iRTime, iNewNLvls, cClouds, &
-                  cUnknown, iLat, cUnknown2
+    i = 1
 
-    call pull_records(unit, iNewNLvls)
+    do 
+        read (unit,*,IOSTAT=reason) cStnid, iCurYear, iCurMon, iCurDay, &
+                                    iCurHour, iRTime, iNewNLvls
 
-    print *, cStnid
+        print *, iNewNLvls, cUnknown2
+ 
+        if (reason < 0) then
+            print *, "Finished program execution."
+            exit
+        else 
+            call pull_records(unit, iNewNLvls)
+            print *, i, "Finished looking at ", cStnid
+            i = i + 1
+        end if
+    end do
+
 end subroutine pull_data_from_file
 
 subroutine pull_records(unit, iNewNLvls)
     implicit none
 
-    integer :: unit, iNewNLvls, i, x
+    integer :: unit, iNewNLvls, i
 
-    character (len=60) :: mystring
+    character (len=100) :: mystring
     character (len=3), dimension(iNewNLvls) :: cLvlTypes
     integer, dimension(iNewNLvls) :: iETimes, iPressures, &
               iHeights, iTemps, iRelHums, iDewDeps, iWDirs, iWSpeeds
 
     !          1         2         3         4         5
     ! 123456789012345678901234567890123456789012345678901234567890
+    ! 21 -9999  99500A-9999   132A-9999    70   360    15
     ! 10 -9999  92500   855A  158A-9999   170    80    93
 
     do i = 1, iNewNLvls
         read (unit, '(A)') mystring
 
-        !x = parse_substring(mystring, 4, 8)
         cLvlTypes(i) = mystring(1:2)
-        !iETimes(i) = parse_substring(mystring, 4, 8)
-        !iPressures(i) = parse_substring(mystring, 11, 15)
-        !iHeights(i) = parse_substring(mystring, 16, 21)
-        !iTemps(i) = parse_substring(mystring, 23, 27)
-        !iRelHums(i) = parse_substring(mystring, 29, 33)
-        !iDewDeps(i) = parse_substring(mystring, 34, 39)
-        !iWDirs(i) = parse_substring(mystring, 40, 45)
-        !iWSpeeds(i) = parse_substring(mystring, 46, 51)
+        iETimes(i) = read_substring(mystring, 4, 8)
+        iPressures(i) = read_substring(mystring, 11, 15)
+        iHeights(i) = read_substring(mystring, 17, 21)
+        iTemps(i) = read_substring(mystring, 23, 27)
+        iRelHums(i) = read_substring(mystring, 29, 33)
+        iDewDeps(i) = read_substring(mystring, 34, 39)
+        iWDirs(i) = read_substring(mystring, 40, 45)
+        iWSpeeds(i) = read_substring(mystring, 46, 51)
     end do
 end subroutine pull_records
 
-integer function parse_substring(mystring, start, end)
+integer function read_substring(mystring, start, end)
     implicit none
 
-    character (len=60) :: mystring, substr
+    character (len=100) :: mystring, substr
     integer :: start, end, ans
 
-    substr = mystring(start, end)
+    substr = mystring(start:end)
     read (substr, *) ans
-    parse_substring = ans
-end function parse_substring
+    read_substring = ans
+end function read_substring
 
 end program for_tant
